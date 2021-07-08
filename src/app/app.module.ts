4daeec14d3bdeffbe9ac8bd7f8dac82a3d2a619c
Cerberus <cerberusprogrammer@gmail.com>
2021-07-07 21:01:08 -0700

    hola

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,29 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Para material design...
+import {MatSliderModule} from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
+    HeroDetailComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule // Se Declara de esta forma la importacion
+    MatToolbarModule,
+    MatCardModule,
+    FormsModule,
+    MatSliderModule, // este igual
+    BrowserAnimationsModule // Se Declara de esta forma la importacion
   ],
   providers: [],
   bootstrap: [AppComponent]
